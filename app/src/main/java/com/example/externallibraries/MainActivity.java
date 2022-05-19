73e18372a8258ca37e01b964d4d8ee02708e11aa
b21leowa <b21leowa@student.his.se>
2022-05-19 12:33:33 +0200

    Can now download image with Glide from URL and put into a ImageView

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.externallibraries;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageOne;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageOne = findViewById(R.id.image_one);
+
+        Glide.with(this).load("https://cdn.pixabay.com/photo/2022/05/08/18/13/cat-7182671_960_720.jpg").into(imageOne);
+
+
     }
 }
